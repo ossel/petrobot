ced5ee16daeb00f23de49529198b1d8940d58381
ossel <ossel@example.com>
2018-06-11 21:51:19 +0200

    Handle todo commands

--- src/main.rs
+++ src/main.rs
@@ -10,8 +10,18 @@ use telegram_bot::*;
 
 mod tests;
 
+static COMMAND_TODO: &'static str = "/todo";
+static COMMAND_TODO_LIST: &'static str = "/todolist";
+static COMMAND_TODO_DELETE: &'static str = "/todo_loesche";
 
 fn main() {
+
+    for (key, value) in env::vars() {
+        if key.starts_with("TELEGRAM_BOT"){
+            println!("{}: {}", key, value);
+        }
+    }
+
     let mut core = Core::new().unwrap();
     let token = env::var("TELEGRAM_BOT_TOKEN").unwrap();
     let api = Api::configure(token).build(core.handle()).unwrap();
@@ -20,9 +30,9 @@ fn main() {
     // cast chat_id into signed integer 64 bit
     let chat_id = chat_id.parse::<i64>().unwrap();
     let chat = ChatId::new(chat_id);
-    api.spawn(chat.text("Wer kümmert sich heute abend um die Enten?"));
+    api.spawn(chat.text("Bin wieder online!"));
 
-    // Fetch new updates via long poll method
+    println!("Fetch new updates via long poll method...");
     let future = api.stream().for_each(|update| {
 
         // If the received update contains a new message...
@@ -34,11 +44,13 @@ fn main() {
                 println!("Chat: {}", chat_id);
                 println!("<{}>: {}",&message.from.first_name, data);
 
-                if data.starts_with("/todo") {
-                    //resident_in_charge = format!("{}",&message.from.first_name.unwrap());
-                    api.spawn(chat.text(format!("{} möchte einen TODO anlegen.",&message.from.first_name)));
+                if data.starts_with(COMMAND_TODO_LIST) {
+                    api.spawn(chat.text("Todo-Liste:leer"));
+                }else if data.starts_with(COMMAND_TODO_DELETE) {
+                    api.spawn(chat.text(format!("{} möchte ein TODO löschen.",&message.from.first_name)));
+                }else if data.starts_with(COMMAND_TODO){
+                    api.spawn(chat.text(format!("{} möchte ein TODO anlegen.",&message.from.first_name)));
                 }
-
                 // Answer message with "Hi".
                 /*api.spawn(message.text_reply(
                     format!("Hi, {}! You just wrote '{}'", &message.from.first_name, data)
