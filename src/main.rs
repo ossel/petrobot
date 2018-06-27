82397e7355602b18470b9e6f3c70bdc292f247ac
ossel <ossel@example.com>
2018-06-27 17:50:58 +0200

    Manage duck points and persist as csv file

--- src/main.rs
+++ src/main.rs
@@ -3,10 +3,16 @@ extern crate telegram_bot;
 extern crate tokio_core;
 
 use std::env;
+use std::fs::File;
+use std::io::prelude::*;
+use std::collections::HashMap;
+use std::fmt;
+use std::fs::OpenOptions;
 
 use futures::Stream;
 use tokio_core::reactor::Core;
 use telegram_bot::*;
+
 
 mod tests;
 
@@ -27,6 +33,78 @@ static COMMAND_DUCK_RESP_CLAIM_M: &'static str = "/entenpapa";
 static COMMAND_DUCK_RESP_CLAIM_F: &'static str = "/entenmama";
 static COMMAND_DUCK_POINTS: &'static str = "/entenpunkte";
 
+struct Mate {
+    name: String,
+    duck_points: i8
+}
+impl Mate {
+    fn to_csv_string(&self) -> String {
+        let mut result = String::new();
+        result.push_str(&self.name);
+        result.push_str(";");
+        result.push_str(&self.duck_points.to_string());
+        result
+    }
+}
+
+impl fmt::Display for Mate {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "{}: {}", self.name, self.duck_points)
+    }
+}
+
+
+fn read_mates() -> HashMap<String,Mate>{
+    let mut mates = HashMap::new();
+    let filename = "roommates.csv";
+    let mut f = File::open(filename).expect("file <roommates.csv> not found");
+    let mut contents = String::new();
+    f.read_to_string(&mut contents)
+        .expect("something went wrong reading the file");
+    for line in contents.split("\n") {
+        if !line.is_empty(){
+            let mate_attributes: Vec<&str> = line.split(";").collect();
+            let name = String::from(mate_attributes[0]);
+            let points = String::from(mate_attributes[1]);
+            let m = Mate {
+                name: name,
+                duck_points: points.parse::<i8>().unwrap()
+            };
+            mates.insert(m.name.clone(),m);
+        }
+    }
+    mates
+}
+
+fn get_mates() -> Vec<String>{
+    let mut mates:Vec<String> = Vec::new();
+    for (_key, value) in read_mates() {
+        mates.push(value.to_string());
+    }
+    mates
+}
+
+fn to_csv_string(mates:HashMap<String,Mate>) -> String{
+    let mut result = String::new();
+    for (_key, value) in mates {
+        result.push_str(&value.to_csv_string());
+        result.push_str("\n");
+    }
+    result
+}
+
+
+fn write_mates(content: String) -> std::io::Result<()> {
+    let mut file = OpenOptions::new()
+        .write(true)
+        .append(false)
+        .open("roommates.csv")
+        .unwrap();
+    println!("write to file: {}", content);
+    file.write_all(content.as_bytes())?;
+    Ok(())
+}
+
 fn main() {
 
     for (key, value) in env::vars() {
@@ -44,7 +122,6 @@ fn main() {
     let chat_id = chat_id.parse::<i64>().unwrap();
     let chat = ChatId::new(chat_id);
     api.spawn(chat.text("Bin wieder online!"));
-
 
     let mut todo_list:Vec<String> = Vec::new();
     let mut shopping_list:Vec<String> = Vec::new();
@@ -73,13 +150,24 @@ fn main() {
                     todo_list.push(todo_task.clone());
                 }else if data.starts_with(COMMAND_POOL_TEMPERATURE){
                     api.spawn(chat.text(format!("Die Pooltemperatur beträgt 20 Grad Celsius.")));
-                    api.spawn(chat.text(format!("... NIIICHT https://www.youtube.com/watch?v=JMAVaefZ384")));
                 }else if data.starts_with(COMMAND_DUCK_CHECK){
                     api.spawn(chat.text(format!("{} ist heute für die Enten zuständig.",duck_father)));
                 }else if data.starts_with(COMMAND_DUCK_RESP_CLAIM_M)||data.starts_with(COMMAND_DUCK_RESP_CLAIM_F){
                     duck_father = message.from.first_name;
+                    let mut mates = read_mates();
+                    let mut points = match mates.get(&duck_father) {
+                        None => 0,
+                        Some(i) => i.duck_points,
+                    };
+                    let m = Mate {
+                        name: duck_father.to_string(),
+                        duck_points: points+1
+                    };
+                    println!("enten punkte {}",m);
+                    mates.insert(m.name.clone(),m);
+                    write_mates(to_csv_string(mates));
                 }else if data.starts_with(COMMAND_DUCK_POINTS){
-                    api.spawn(chat.text("Noch nicht implementiert."));
+                    api.spawn(chat.text(format!("{}",to_ordered_list_string(get_mates()))));
                 }else if data.starts_with(COMMAND_SHOPPING_LIST_DELETE)||data.starts_with(COMMAND_SHOPPING_LIST_DELETE_TYPO){
                     shopping_list.clear();
                     api.spawn(chat.text("Liste gelöscht."));
@@ -91,7 +179,6 @@ fn main() {
                     api.spawn(chat.text(format!("{}",to_ordered_list_string(shopping_list.clone()))));
                 }else if data.starts_with(COMMAND_SHOPPING_ITEM){
                     let chat_input_string = format!("{}",&data.clone());
-                    //let mut split = ;
                     let command_and_item: Vec<&str> = chat_input_string.split(COMMAND_SHOPPING_ITEM).collect();
                     let mut item = String::from(command_and_item[1]);
                     item.push_str(&format!(" ({})",message.from.first_name));
