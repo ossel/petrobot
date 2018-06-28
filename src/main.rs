da9282a96aa24f396052c86baee448de487d1821
ossel <ossel@example.com>
2018-06-28 18:41:35 +0200

    Persist all data

--- src/main.rs
+++ src/main.rs
@@ -9,6 +9,7 @@ use std::io::prelude::*;
 use std::collections::HashMap;
 use std::fmt;
 use std::fs::OpenOptions;
+use std::fs;
 
 use futures::Stream;
 use tokio_core::reactor::Core;
@@ -59,7 +60,15 @@ impl fmt::Display for Mate {
 fn read_mates() -> HashMap<String,Mate>{
     let mut mates = HashMap::new();
     let filename = "roommates.csv";
-    let mut f = File::open(filename).expect("file <roommates.csv> not found");
+    let mut f = match File::open(filename) {
+        Ok(file) => file,
+        Err(_) => {
+            File::create(filename).expect("Could not create file <roommates.csv>");
+            let result = File::open(filename).expect("Could not open newly created file <roommates.csv>");
+            result
+        },
+    };
+
     let mut contents = String::new();
     f.read_to_string(&mut contents)
         .expect("something went wrong reading the file");
@@ -78,12 +87,63 @@ fn read_mates() -> HashMap<String,Mate>{
     mates
 }
 
+fn write_mates(content: String) {
+    let mut file = OpenOptions::new()
+        .write(true)
+        .append(false)
+        .open("roommates.csv")
+        .unwrap();
+    match file.write_all(content.as_bytes()){
+        Ok(_) => println!("Updated <roommates.csv> : {}", content),
+        Err(_) => println!("Could not write to file <roommates.csv> : {}", content),
+    };
+}
+
 fn get_mates() -> Vec<String>{
     let mut mates:Vec<String> = Vec::new();
     for (_key, value) in read_mates() {
         mates.push(value.to_string());
     }
     mates
+}
+
+fn read(filename:String) -> Vec<String>{
+    let mut f = match File::open(filename.clone()) {
+        Ok(file) => file,
+        Err(_) => {
+            File::create(filename.clone()).expect("Could not create file");
+            let result = File::open(filename.clone()).expect("Could not open newly created file");
+            result
+        },
+    };
+
+    let mut contents = String::new();
+    f.read_to_string(&mut contents)
+        .expect("something went wrong reading the file");
+    let mut result:Vec<String> = Vec::new();
+    for value in contents.split(";"){
+        if !value.is_empty(){
+            result.push(value.to_string());
+        }
+    }
+    result
+}
+
+fn write(filename: String, data:Vec<String>) {
+    let mut content = String::new();
+    for value in data {
+        content.push_str(&value);
+        content.push_str(";");
+    }
+    let mut file = OpenOptions::new()
+        .write(true)
+        .append(false)
+        .open(filename)
+        .unwrap();
+    match file.write_all(content.as_bytes()){
+        Ok(_) => println!("Updated file: {}", content),
+        Err(_) => println!("Could not write to file: {}", content),
+    };
 }
 
 fn to_csv_string(mates:HashMap<String,Mate>) -> String{
@@ -95,17 +155,6 @@ fn to_csv_string(mates:HashMap<String,Mate>) -> String{
     result
 }
 
-
-fn write_mates(content: String) -> std::io::Result<()> {
-    let mut file = OpenOptions::new()
-        .write(true)
-        .append(false)
-        .open("roommates.csv")
-        .unwrap();
-    println!("write to file: {}", content);
-    file.write_all(content.as_bytes())?;
-    Ok(())
-}
 
 fn main() {
 
@@ -125,10 +174,10 @@ fn main() {
     let chat = ChatId::new(chat_id);
     api.spawn(chat.text("Bin wieder online!"));
 
-    let mut todo_list:Vec<String> = Vec::new();
-    let mut shopping_list:Vec<String> = Vec::new();
     let mut duck_father = String::new();
     let mut duck_father_claim_time = Local::now().date().pred();
+
+
     println!("// Fetch new updates via long poll method...");
     let future = api.stream().for_each(|update| {
 
@@ -140,16 +189,25 @@ fn main() {
                 println!("<{}>: {}",&message.from.first_name, data);
 
                 if data.starts_with(COMMAND_TODO_LIST) {
-                    api.spawn(chat.text(format!("{}",to_ordered_list_string(todo_list.clone()))));
+                    api.spawn(chat.text(format!("{}",to_ordered_list_string(read("todo-list.csv".to_string())))));
                 }else if data.starts_with(COMMAND_TODO_DELETE) {
-                    api.spawn(chat.text(format!("{} möchte einen TODO löschen. Letzter Eintrag gelöscht.",&message.from.first_name)));
+                    api.spawn(chat.text(format!("Letzter Eintrag gelöscht.")));
+                    let mut todo_list = read("todo-list.csv".to_string());
                     todo_list.pop();
+                    match fs::remove_file("todo-list.csv"){
+                        Ok(_) => println!("todo-list.csv removed."),
+                        Err(_) => println!("Could not remove todo-list.csv"),
+                    };
+                    File::create("todo-list.csv").expect("Could not create file");
+                    write("todo-list.csv".to_string(),todo_list);
                 }else if data.starts_with(COMMAND_TODO){
                     let chat_input_string = format!("{}",&data.clone());
                     //let mut split = ;
                     let command_and_task: Vec<&str> = chat_input_string.split(COMMAND_TODO).collect();
                     let mut todo_task = String::from(command_and_task[1]);
+                    let mut todo_list = read("todo-list.csv".to_string());
                     todo_list.push(todo_task.clone());
+                    write("todo-list.csv".to_string(),todo_list);
                 }else if data.starts_with(COMMAND_POOL_TEMPERATURE){
                     api.spawn(chat.text(format!("Die Pooltemperatur beträgt 20 Grad Celsius.")));
                 }else if data.starts_with(COMMAND_DUCK_CHECK){
@@ -178,27 +236,33 @@ fn main() {
                 }else if data.starts_with(COMMAND_DUCK_POINTS){
                     api.spawn(chat.text(format!("{}",to_ordered_list_string(get_mates()))));
                 }else if data.starts_with(COMMAND_SHOPPING_LIST_DELETE)||data.starts_with(COMMAND_SHOPPING_LIST_DELETE_TYPO){
-                    shopping_list.clear();
+                    write("shopping-list-old.csv".to_string(),read("shopping-list.csv".to_string()));
+                    fs::remove_file("shopping-list.csv").expect("Could not remove list");
                     api.spawn(chat.text("Liste gelöscht."));
                 }else if (data.starts_with(COMMAND_SHOPPING_LIST)
                     || data.starts_with(COMMAND_SHOPPING_LIST_TYPO) )
-                    && shopping_list.is_empty(){
+                    && read("shopping-list.csv".to_string()).is_empty(){
                     api.spawn(chat.text("Einkaufsliste leer. Tippe '/einkauf <item>' um etwas hinzuzufügen."));
                 }else if data.starts_with(COMMAND_SHOPPING_LIST) || data.starts_with(COMMAND_SHOPPING_LIST_TYPO) {
-                    api.spawn(chat.text(format!("{}",to_ordered_list_string(shopping_list.clone()))));
+                    api.spawn(chat.text(format!("{}",to_ordered_list_string(read("shopping-list.csv".to_string())))));
                 }else if data.starts_with(COMMAND_SHOPPING_ITEM){
                     let chat_input_string = format!("{}",&data.clone());
                     let command_and_item: Vec<&str> = chat_input_string.split(COMMAND_SHOPPING_ITEM).collect();
-                    let mut item = String::from(command_and_item[1]);
+                    let mut item = String::new();
+                    if command_and_item[1].starts_with("@Petrobot") {
+                        let prefix_and_item: Vec<&str> = command_and_item[1].split("@Petrobot").collect();
+                        item.push_str(prefix_and_item[1]);
+                    }else{
+                        item.push_str(command_and_item[1]);
+                    }
+
                     item.push_str(&format!(" ({})",message.from.first_name));
+                    let mut shopping_list = read("shopping-list.csv".to_string());
                     shopping_list.push(item.clone());
+                    write("shopping-list.csv".to_string(),shopping_list);
                 }else{
                     println!("Command '{}' unknown.",data);
                 }
-                /* Answer message with "Hi".
-                api.spawn(message.text_reply(
-                    format!("Hi, {}! You just wrote '{}'", &message.from.first_name, data)
-                ));*/
             }
         }
 
@@ -217,3 +281,4 @@ fn to_ordered_list_string(list:Vec<String>) -> String{
     }
     result
 }
+
