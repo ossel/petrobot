7b55b9eafb512cf5b698d45708470b5ed594d4e1
ossel <ossel@example.com>
2018-07-05 16:58:24 +0200

    Move csv files to data dir and seperate DAO code

---
+++ src/dao.rs
@@ -0,0 +1,150 @@
+use std::fmt;
+
+use std::fs::File;
+use std::fs;
+use std::io::prelude::*;
+use std::collections::HashMap;
+use std::fs::OpenOptions;
+
+
+pub struct Mate {
+    pub name: String,
+    pub duck_points: i8
+}
+impl Mate {
+    pub fn to_csv_string(&self) -> String {
+        let mut result = String::new();
+        result.push_str(&self.name);
+        result.push_str(";");
+        result.push_str(&self.duck_points.to_string());
+        result
+    }
+}
+
+impl fmt::Display for Mate {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "{}: {}", self.name, self.duck_points)
+    }
+}
+
+
+pub fn read_mates() -> HashMap<String,Mate>{
+    let mut mates = HashMap::new();
+    let filename = "data/roommates.csv";
+    let mut f = match File::open(filename) {
+        Ok(file) => file,
+        Err(_) => {
+            File::create(filename).expect("Could not create file <roommates.csv>");
+            let result = File::open(filename).expect("Could not open newly created file <roommates.csv>");
+            result
+        },
+    };
+
+    let mut contents = String::new();
+    f.read_to_string(&mut contents)
+        .expect("something went wrong reading the file");
+    for line in contents.split("\n") {
+        if !line.is_empty(){
+            let mate_attributes: Vec<&str> = line.split(";").collect();
+            let name = String::from(mate_attributes[0]);
+            let points = String::from(mate_attributes[1]);
+            let m = Mate {
+                name: name,
+                duck_points: points.parse::<i8>().unwrap()
+            };
+            mates.insert(m.name.clone(),m);
+        }
+    }
+    mates
+}
+
+pub fn write_mates(mates: HashMap<String,Mate>) {
+    let content = to_csv_string(mates);
+    let mut file = OpenOptions::new()
+        .write(true)
+        .append(false)
+        .open("data/roommates.csv")
+        .unwrap();
+    match file.write_all(content.as_bytes()){
+        Ok(_) => info!("Updated <roommates.csv> : {}", content),
+        Err(_) => info!("Could not write to file <roommates.csv> : {}", content),
+    };
+}
+
+pub fn get_mates() -> Vec<String>{
+    let mut mates:Vec<String> = Vec::new();
+    for (_key, value) in read_mates() {
+        mates.push(value.to_string());
+    }
+    mates
+}
+
+fn read(filename:String) -> Vec<String>{
+    let mut f = match File::open(filename.clone()) {
+        Ok(file) => file,
+        Err(_) => {
+            File::create(filename.clone()).expect("Could not create file");
+            let result = File::open(filename.clone()).expect("Could not open newly created file");
+            result
+        },
+    };
+
+    let mut contents = String::new();
+    f.read_to_string(&mut contents)
+        .expect("something went wrong reading the file");
+    let mut result:Vec<String> = Vec::new();
+    for value in contents.split(";"){
+        if !value.is_empty(){
+            result.push(value.to_string());
+        }
+    }
+    result
+}
+
+pub fn read_shopping_list() -> Vec<String>{
+    read("data/shopping-list.csv".to_string())
+}
+
+pub fn write_shopping_list(data:Vec<String>){
+    write("data/shopping-list.csv".to_string(),data)
+}
+
+pub fn delete_shopping_list(){
+    write("data/shopping-list-old.csv".to_string(),read_shopping_list());
+    fs::remove_file("data/shopping-list.csv").expect("Could not remove shopping-list.csv file");
+}
+
+pub fn read_todo_list() -> Vec<String>{
+    read("data/todo-list.csv".to_string())
+}
+
+pub fn write_todo_list(data:Vec<String>){
+    write("data/todo-list.csv".to_string(),data)
+}
+
+
+fn write(filename: String, data:Vec<String>) {
+    let mut content = String::new();
+    for value in data {
+        content.push_str(&value);
+        content.push_str(";");
+    }
+    let mut file = OpenOptions::new()
+        .write(true)
+        .append(false)
+        .open(filename)
+        .unwrap();
+    match file.write_all(content.as_bytes()){
+        Ok(_) => info!("Updated file: {}", content),
+        Err(_) => info!("Could not write to file: {}", content),
+    };
+}
+
+fn to_csv_string(mates:HashMap<String,Mate>) -> String{
+    let mut result = String::new();
+    for (_key, value) in mates {
+        result.push_str(&value.to_csv_string());
+        result.push_str("\n");
+    }
+    result
+}
--- src/main.rs
+++ src/main.rs
@@ -7,10 +7,6 @@ extern crate log4rs;
 
 use std::env;
 use std::fs::File;
-use std::io::prelude::*;
-use std::collections::HashMap;
-use std::fmt;
-use std::fs::OpenOptions;
 use std::fs;
 
 use futures::Stream;
@@ -24,6 +20,7 @@ use log4rs::config::{Appender, Config, Root};
 
 
 mod tests;
+mod dao;
 
 static COMMAND_TODO: &'static str = "/todo";
 static COMMAND_TODO_LIST: &'static str = "/todolist";
@@ -41,125 +38,6 @@ static COMMAND_DUCK_CHECK: &'static str = "/entendienst";
 static COMMAND_DUCK_RESP_CLAIM_M: &'static str = "/entenpapa";
 static COMMAND_DUCK_RESP_CLAIM_F: &'static str = "/entenmama";
 static COMMAND_DUCK_POINTS: &'static str = "/entenpunkte";
-
-struct Mate {
-    name: String,
-    duck_points: i8
-}
-impl Mate {
-    fn to_csv_string(&self) -> String {
-        let mut result = String::new();
-        result.push_str(&self.name);
-        result.push_str(";");
-        result.push_str(&self.duck_points.to_string());
-        result
-    }
-}
-
-impl fmt::Display for Mate {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        write!(f, "{}: {}", self.name, self.duck_points)
-    }
-}
-
-
-fn read_mates() -> HashMap<String,Mate>{
-    let mut mates = HashMap::new();
-    let filename = "roommates.csv";
-    let mut f = match File::open(filename) {
-        Ok(file) => file,
-        Err(_) => {
-            File::create(filename).expect("Could not create file <roommates.csv>");
-            let result = File::open(filename).expect("Could not open newly created file <roommates.csv>");
-            result
-        },
-    };
-
-    let mut contents = String::new();
-    f.read_to_string(&mut contents)
-        .expect("something went wrong reading the file");
-    for line in contents.split("\n") {
-        if !line.is_empty(){
-            let mate_attributes: Vec<&str> = line.split(";").collect();
-            let name = String::from(mate_attributes[0]);
-            let points = String::from(mate_attributes[1]);
-            let m = Mate {
-                name: name,
-                duck_points: points.parse::<i8>().unwrap()
-            };
-            mates.insert(m.name.clone(),m);
-        }
-    }
-    mates
-}
-
-fn write_mates(content: String) {
-    let mut file = OpenOptions::new()
-        .write(true)
-        .append(false)
-        .open("roommates.csv")
-        .unwrap();
-    match file.write_all(content.as_bytes()){
-        Ok(_) => info!("Updated <roommates.csv> : {}", content),
-        Err(_) => info!("Could not write to file <roommates.csv> : {}", content),
-    };
-}
-
-fn get_mates() -> Vec<String>{
-    let mut mates:Vec<String> = Vec::new();
-    for (_key, value) in read_mates() {
-        mates.push(value.to_string());
-    }
-    mates
-}
-
-fn read(filename:String) -> Vec<String>{
-    let mut f = match File::open(filename.clone()) {
-        Ok(file) => file,
-        Err(_) => {
-            File::create(filename.clone()).expect("Could not create file");
-            let result = File::open(filename.clone()).expect("Could not open newly created file");
-            result
-        },
-    };
-
-    let mut contents = String::new();
-    f.read_to_string(&mut contents)
-        .expect("something went wrong reading the file");
-    let mut result:Vec<String> = Vec::new();
-    for value in contents.split(";"){
-        if !value.is_empty(){
-            result.push(value.to_string());
-        }
-    }
-    result
-}
-
-fn write(filename: String, data:Vec<String>) {
-    let mut content = String::new();
-    for value in data {
-        content.push_str(&value);
-        content.push_str(";");
-    }
-    let mut file = OpenOptions::new()
-        .write(true)
-        .append(false)
-        .open(filename)
-        .unwrap();
-    match file.write_all(content.as_bytes()){
-        Ok(_) => info!("Updated file: {}", content),
-        Err(_) => info!("Could not write to file: {}", content),
-    };
-}
-
-fn to_csv_string(mates:HashMap<String,Mate>) -> String{
-    let mut result = String::new();
-    for (_key, value) in mates {
-        result.push_str(&value.to_csv_string());
-        result.push_str("\n");
-    }
-    result
-}
 
 
 fn main() {
@@ -210,25 +88,25 @@ fn main() {
                 info!("<{}>: {}",&message.from.first_name, data);
 
                 if data.starts_with(COMMAND_TODO_LIST) {
-                    api.spawn(chat.text(format!("{}",to_ordered_list_string(read("todo-list.csv".to_string())))));
+                    api.spawn(chat.text(format!("{}",to_ordered_list_string(dao::read_todo_list()))));
                 }else if data.starts_with(COMMAND_TODO_DELETE) {
                     api.spawn(chat.text(format!("Letzter Eintrag gelöscht.")));
-                    let mut todo_list = read("todo-list.csv".to_string());
+                    let mut todo_list = dao::read_todo_list();
                     todo_list.pop();
                     match fs::remove_file("todo-list.csv"){
                         Ok(_) => info!("todo-list.csv removed."),
                         Err(_) => info!("Could not remove todo-list.csv"),
                     };
                     File::create("todo-list.csv").expect("Could not create file");
-                    write("todo-list.csv".to_string(),todo_list);
+                    dao::write_todo_list(todo_list);
                 }else if data.starts_with(COMMAND_TODO){
                     let chat_input_string = format!("{}",&data.clone());
                     //let mut split = ;
                     let command_and_task: Vec<&str> = chat_input_string.split(COMMAND_TODO).collect();
                     let mut todo_task = String::from(command_and_task[1]);
-                    let mut todo_list = read("todo-list.csv".to_string());
+                    let mut todo_list = dao::read_todo_list();
                     todo_list.push(todo_task.clone());
-                    write("todo-list.csv".to_string(),todo_list);
+                    dao::write_todo_list(todo_list);
                 }else if data.starts_with(COMMAND_POOL_TEMPERATURE){
                     api.spawn(chat.text(format!("Die Pooltemperatur beträgt 20 Grad Celsius.")));
                 }else if data.starts_with(COMMAND_DUCK_CHECK){
@@ -241,31 +119,30 @@ fn main() {
                         duck_father_claim_time = today;
                         duck_father = message.from.first_name;
                         api.spawn(chat.text(String::from("Du bist heute für die Enten zuständig.")));
-                        let mut mates = read_mates();
+                        let mut mates = dao::read_mates();
                         let mut points = match mates.get(&duck_father) {
                             None => 0,
                             Some(i) => i.duck_points,
                         };
-                        let m = Mate {
+                        let m = dao::Mate {
                             name: duck_father.to_string(),
                             duck_points: points+1
                         };
                         mates.insert(m.name.clone(),m);
-                        write_mates(to_csv_string(mates));
+                        dao::write_mates(mates);
                     }
 
                 }else if data.starts_with(COMMAND_DUCK_POINTS){
-                    api.spawn(chat.text(format!("{}",to_ordered_list_string(get_mates()))));
+                    api.spawn(chat.text(format!("{}",to_ordered_list_string(dao::get_mates()))));
                 }else if data.starts_with(COMMAND_SHOPPING_LIST_DELETE)||data.starts_with(COMMAND_SHOPPING_LIST_DELETE_TYPO){
-                    write("shopping-list-old.csv".to_string(),read("shopping-list.csv".to_string()));
-                    fs::remove_file("shopping-list.csv").expect("Could not remove list");
+                    dao::delete_shopping_list();
                     api.spawn(chat.text("Liste gelöscht."));
                 }else if (data.starts_with(COMMAND_SHOPPING_LIST)
                     || data.starts_with(COMMAND_SHOPPING_LIST_TYPO) )
-                    && read("shopping-list.csv".to_string()).is_empty(){
+                    && dao::read_shopping_list().is_empty(){
                     api.spawn(chat.text("Einkaufsliste leer. Tippe '/einkauf <item>' um etwas hinzuzufügen."));
                 }else if data.starts_with(COMMAND_SHOPPING_LIST) || data.starts_with(COMMAND_SHOPPING_LIST_TYPO) {
-                    api.spawn(chat.text(format!("{}",to_ordered_list_string(read("shopping-list.csv".to_string())))));
+                    api.spawn(chat.text(format!("{}",to_ordered_list_string(dao::read_shopping_list()))));
                 }else if data.starts_with(COMMAND_SHOPPING_ITEM){
                     let chat_input_string = format!("{}",&data.clone());
                     let command_and_item: Vec<&str> = chat_input_string.split(COMMAND_SHOPPING_ITEM).collect();
@@ -278,9 +155,9 @@ fn main() {
                     }
 
                     item.push_str(&format!(" ({})",message.from.first_name));
-                    let mut shopping_list = read("shopping-list.csv".to_string());
+                    let mut shopping_list = dao::read_shopping_list();
                     shopping_list.push(item.clone());
-                    write("shopping-list.csv".to_string(),shopping_list);
+                    dao::write_shopping_list(shopping_list);
                 }else{
                     info!("Command '{}' unknown.",data);
                 }
